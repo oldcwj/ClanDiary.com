35fe6a2f21b294a452968b46a34d50c628b75b93
cuiwj <cuiwj@cuiwjmatoMacBook-Pro.local>
2015-03-08 09:19:10 +0800

    add not found page

---
+++ app/Global.java
@@ -0,0 +1,26 @@
+import models.Item;
+import play.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import play.libs.F.*;
+
+import static play.mvc.Results.*;
+
+public class Global extends GlobalSettings {
+    
+    @Override
+    public void onStart(Application app) {
+        super.onStart(app);
+        if (Item.find.all().isEmpty()) {
+        }
+    }
+
+    @Override
+    public Promise<Result> onHandlerNotFound(RequestHeader request) {
+        return Promise.<Result>pure(notFound(
+                views.html.notFoundPage.render()
+            ));
+    }
+    
+    
+}
